3f56a0ce0768973f59258d37023c08fa7bf2c739
Your Name <you@example.com>
2025-11-11 21:10:37 +0900

    first

--- LSH_portfolio/01_samdasoo/js/sub.js
+++ LSH_portfolio/01_samdasoo/js/sub.js
@@ -6,3 +6,19 @@ btn.addEventListener("click", (e) => {
 
   con.classList.toggle("on");
 });
+AOS.init({
+  //offset: 120, 기본값
+  delay: 1000,
+  easing: "ease",
+  duration: 1000,
+  once: false,
+});
+
+const swiper = new Swiper(".subtxt", {
+  slidesPerView: 1,
+  speed: 800,
+  navigation: {
+    nextEl: ".bright",
+    prevEl: ".bleft",
+  },
+});
