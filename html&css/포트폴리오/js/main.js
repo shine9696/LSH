49ad83916053c64dfce2b2d0c764213bf41ef444
Your Name <you@example.com>
2025-12-16 17:58:37 +0900

    first

---
+++ html&css/포트폴리오/js/main.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  resizeObserver: false,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
